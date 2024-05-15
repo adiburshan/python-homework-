76f494da0baea6f9b864d4104c22a5347ff8909c
adiburshan <165251972+adiburshan@users.noreply.github.com>
2024-05-15 19:50:07 +0300

    Add files via upload
    enjoy my python hw

---
+++ calcs.py
@@ -0,0 +1,5 @@
+def plus(n1=2 , n2=7):
+    return n1 + n2
+
+def kefel(n3=5 , n4=2):
+    return n3 * n4
---
+++ hw.py
@@ -0,0 +1,6 @@
+from calcs import plus,kefel
+from icecream import ic
+
+if __name__ == "__main__":
+    ic(plus(3,4))
+    ic(kefel())
